d329039b21ec87883cc39b94181dabc6ddf621c5
Britikoos <101356915+Britikoos@users.noreply.github.com>
2023-12-18 10:56:04 +0300

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,16 +1,25 @@
 (function() {
-  // Функция для измерения времени загрузки страницы
-  var startTime = performance.now();
+  // Создаем начальную метку "startLoad"
+  performance.mark('startLoad');
 
   // Событие загрузки страницы
   window.addEventListener('load', function() {
-    var endTime = performance.now();
-    var loadTime = endTime - startTime;
+    // Создаем конечную метку "endLoad"
+    performance.mark('endLoad');
 
-    // Получаем элемент footer по его id
-    var footer = document.getElementById('pageFooter');
+    // Измеряем время между начальной и конечной метками
+    performance.measure('pageLoadTime', 'startLoad', 'endLoad');
+    
+    // Получаем результат измерения
+    var measures = performance.getEntriesByName('pageLoadTime');
+    if (measures.length > 0) {
+      var loadTime = measures[0].duration;
 
-    // Вставляем информацию о времени загрузки в подвал
-    footer.innerHTML = 'Время загрузки страницы: ' +  loadTime + ' ms';
+      // Получаем элемент footer по его id
+      var footer = document.getElementById('pageFooter');
+
+      // Вставляем информацию о времени загрузки в подвал
+      footer.innerHTML = 'Время загрузки страницы: ' + loadTime.toFixed(2) + ' ms';
+    }
   });
 })();
