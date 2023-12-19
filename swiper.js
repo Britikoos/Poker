ff328d88d23e100d540334b201664412b4342c29
Britikoos <101356915+Britikoos@users.noreply.github.com>
2023-12-19 13:18:21 +0300

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -1,9 +1,17 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  centeredSlides: true,
+  slidesPerView: 2, // Количество отображаемых слайдов одновременно
+  spaceBetween: 30, // Расстояние между слайдами
+  centeredSlides: true, // Активация режима центрирования слайдов
+
+  // Опции для пагинации (точек под слайдером)
+  pagination: {
+    el: ".swiper-pagination", // где будут добавлены точки пагинации
+    clickable: true, // Включение возможности клика по точкам для переключения слайдов
+  },
+  // Опции для навигации (кнопок "Next" и "Prev")
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
+
